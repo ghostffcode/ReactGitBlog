81a38c539cd06617fb7c8fa6f39f3b091053513a
PoeticBliss <meremezeukah@gmail.com>
2016-08-02 15:00:15 -0400

    removed config file

--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  Key: "AIzaSyCaTbuPESNwOw9s-zmvJ4gb7BwRFvhKzf4",
-  Domain: "gitblog-d68cd.firebaseapp.com",
-  DB: "https://gitblog-d68cd.firebaseio.com",
-  Storage: "gitblog-d68cd.appspot.com"
-};
